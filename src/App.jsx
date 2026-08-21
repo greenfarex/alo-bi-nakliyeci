4c97774584f3a1f435129abf2c173b017794a387
Musa <133766318+greenfarex@users.noreply.github.com>
2026-08-22 01:54:07 +0300

    Add tracking to WhatsApp link click event

--- src/App.jsx
+++ src/App.jsx
@@ -403,6 +403,7 @@ function App() {
         href="https://wa.me/905342359168?text=Merhaba%2C%20Alo%20Bi%20Nakliyeci%27den%20nakliye%20fiyat%C4%B1%20almak%20istiyorum.%0A%0ANereden%3A%20%0ANereye%3A%20%0AY%C3%BCk%C3%BCn%20t%C3%BCr%C3%BC%3A"
         target="_blank"
         rel="noopener noreferrer"
+        onClick={() => trackWhatsApp('floating')}
         style={{
           position: "fixed",
           right: "25px",
